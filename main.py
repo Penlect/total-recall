29246115373faa1f005b75e52ec65b98e8ba864a
Penlect <daniel.4ndersson@gmail.com>
2017-06-19 21:44:35 +0200

    Added simple key logic and computed nr correct/wrong/none recalled.

--- main.py
+++ main.py
@@ -2,20 +2,38 @@
 from flask import Flask, render_template, request, jsonify
 
 from random import randint
+from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route('/numbers')
 def numbers():
-    return render_template('numbers.html', nr_rows=5, nr_cols=10)
+    key = 'abc123'
+    data = [str(randint(0, 9)) for _ in range(5*10)]
+    with open(f'database/{key}.txt', 'w') as file:
+        file.write(';'.join(data))
+    return render_template('numbers.html', nr_rows=5, nr_cols=10, key=key)
+
+
+def _arbeiter(key, dictionary):
+    pass
+
 
 @app.route('/arbeiter', methods=['POST'])
 def arbeiter():
     if request.method == 'POST':
-
         d = dict(request.form)
-        for key in d:
-            d[key] = bool(randint(0, 1))
-        return jsonify(d)
+        resp = dict()
+        key = 'abc123'
+        with open(f'database/{key}.txt', 'r') as file:
+            data = file.read().split(';')
+            for key in d:
+                id_ = int(key.split('_')[-1].strip())
+                if d[key][0].strip():
+                    resp[key] = data[id_] == d[key][0]
+                else:
+                    resp[key] = None
+        app.logger.info(resp)
+        return jsonify(resp)
     else:
         print('Wrong method!')
