316c33018c2324f58fa25b0b29088c2970f0b850
Penlect <daniel.4ndersson@gmail.com>
2017-06-19 19:55:49 +0200

    Improved css and Submit will now set random cells to 0

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+
+from random import randint
 
 app = Flask(__name__)
 
 @app.route('/numbers')
 def numbers():
-    return render_template('numbers.html', nr_rows=15, nr_cols=5)
+    return render_template('numbers.html', nr_rows=3, nr_cols=10)
+
+@app.route('/arbeiter', methods=['POST'])
+def arbeiter():
+    if request.method == 'POST':
+
+        d = dict(request.form)
+        for key in d:
+            d[key] = bool(randint(0, 1))
+        return jsonify(d)
+    else:
+        print('Wrong method!')
