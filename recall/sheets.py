3f3c0fedb8bcf50d044d4020d2021ed863c181f6
Penlect <daniel.4ndersson@gmail.com>
2017-11-28 22:51:33 +0100

    Added grid function for horizontal or vertical grid creation

--- recall/sheets.py
+++ recall/sheets.py
@@ -24,20 +24,22 @@ height = 1 - TOP_MARGIN - BOTTOM_MARGIN
 
 ax = fig.add_axes([left, right, width, height])
 
-ax.set_axis_off()
+# ax.set_axis_off()
 # ax.plot([0, 1], [0, 1])
 ax.set_xlim(0, 1)
-ax.set_ylim(0, 1)
+ax.set_ylim(1, 0)
+
+BINARY = slice(5, -5)
 
 nr_rows = 25
 nr_cols = 40
-_pos_y = np.linspace(1, 0, nr_rows + 2)
-_pos_x = np.linspace(0, 0.9, nr_cols + 2)
+_pos_y = np.linspace(0, 1, nr_rows + 2)
+_pos_x = np.linspace(0, 0.9, nr_cols + 2)[BINARY]
 dy = abs(_pos_y[1] - _pos_y[0])
 dx = abs(_pos_x[1] - _pos_x[0])
 item_pos_y = _pos_y[1:-1]
 item_pos_x = _pos_x[1:-1]
-border_pos_y = _pos_y[0:-1] - H_LINES_ADJUSTMENT*dy/2
+border_pos_y = _pos_y[0:-1] + H_LINES_ADJUSTMENT*dy/2
 border_pos_x = _pos_x[0:-1] + dx/2
 
 fp = FontProperties(family=['Arial'])
@@ -45,7 +47,7 @@ row_count_font = FontProperties(family=['Arial'], size='small')
 
 # ROW ENUMERATION
 for i, y in enumerate(item_pos_y, start=1):
-    ax.text(0.915, y, f'Row {i}', fontproperties=row_count_font, ha='left')
+    ax.text(max(_pos_x) + 0.015, y, f'Row {i}', fontproperties=row_count_font, ha='left')
 
 # ITEM DATA GRID
 for y in item_pos_y:
@@ -54,65 +56,53 @@ for y in item_pos_y:
 
 
 def pairs(seq, step=1):
+    seq = list(seq)
     for i in range(0, len(seq) - step, step):
         yield (seq[i], seq[i + step])
-    if seq[i + step] != seq[-1]:
+    if seq and seq[i + step] != seq[-1]:
         yield (seq[i + step], seq[-1])
-
-pattern = [3]
-width = 3
-height = 1
-
-# H LINES
 
 
 def get_boundary_finder(pattern):
     pattern_sum = sum(pattern)
     boundaries = {s - 1 for s in itertools.accumulate(pattern)}
     def boundary(cell_i):
-        return cell_i%pattern_sum in boundaries
+        return cell_i < 0 or cell_i%pattern_sum in boundaries
     return boundary
 
-boundary = get_boundary_finder(pattern)
 
-for i, x in enumerate(border_pos_x):
-    if i%width == 0:
-        ax.plot([x, x], [min(border_pos_y), max(border_pos_y)], lw=0.1, color='black')
-
-lines = list()
-for row_i, (y0, y1) in enumerate(pairs(border_pos_y, step=height), start=0):
-    for col_i, (x0, x1) in enumerate(pairs(border_pos_x, step=width), start=0):
-        cell_i = nr_rows * col_i + row_i
-        print(cell_i, boundary(cell_i))
-        if cell_i == 0:
-            lines.append(([x0, y0], [x1, y0]))
-        elif row_i == 0 and boundary(cell_i - 1):
-            lines.append(([x0, y0], [x1, y0]))
-        elif boundary(cell_i):
-            lines.append(([x0, y1], [x1, y1]))
-ax.add_collection(LineCollection(lines, lw=0.1, color='black'))
-
-# V LINES
-def lol():
-    for i, y in enumerate(border_pos_y):
-        if i%height == 0:
-            ax.plot([min(border_pos_x), max(border_pos_x)], [y, y], lw=0.1, color='black')
-
+def grid(border_pos_x, border_pos_y, pattern, size, direction='horizontal'):
+    if direction == 'horizontal':
+        x, y = border_pos_x, border_pos_y
+    elif direction == 'vertical':
+        x, y = border_pos_y, border_pos_x
+    else:
+        raise ValueError(f'Invalid direction: {direction}')
+    boundary = get_boundary_finder(pattern)
     lines = list()
-    for col_i, (x0, x1) in enumerate(pairs(border_pos_x, step=width), start=0):
-        for row_i, (y0, y1) in enumerate(pairs(border_pos_y, step=height), start=0):
-            cell_i = nr_cols*row_i + col_i
-            if cell_i == 0:
-                lines.append(([x0, y0], [x0, y1]))
-            elif col_i == 0 and boundary(cell_i - 1):
-                lines.append(([x0, y0], [x0, y1]))
-            elif boundary(cell_i):
-                lines.append(([x1, y0], [x1, y1]))
+    for row_i, (y0, y1) in enumerate(pairs(y, step=size), start=0):
+        for col_i, (x0, x1) in enumerate(pairs(x, step=1), start=0):
+            cell_i = (len(x) - 1) * row_i + col_i
+            if col_i == 0 and boundary(cell_i - 1):
+                lines.append(np.array([[x0, y0], [x0, y1]]))
+            if boundary(cell_i):
+                lines.append(np.array([[x1, y0], [x1, y1]]))
+        if row_i == 0:
+            lines.append(np.array([[min(x), y0], [max(x), y0]]))
+        lines.append(np.array([[min(x), y1], [max(x), y1]]))
+
+    if direction == 'vertical':
+        transform = np.array([[0, 1],
+                              [1, 0]])
+        for index in range(len(lines)):
+            lines[index] = np.dot(lines[index], transform)
+
     ax.add_collection(LineCollection(lines, lw=0.1, color='black'))
+
+grid(border_pos_x, border_pos_y, [3], 3, 'vertical')
 
 pp.savefig(fig, figsize=(8.27, 11.69))
 fig.clear()
-
 
 pp.savefig(fig, figsize=(8.27, 11.69))
 
